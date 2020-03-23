875dbb7001f048241b9ec4bb9641900e39e28aca
qq1972474341 <1972474341@qq.com>
2020-03-23 14:52:30 +0800

    增加

--- musicManager/application/common/model/User.php
+++ musicManager/application/common/model/User.php
@@ -8,6 +8,18 @@ class User extends Model
 {
     //开启自动时间戳
     protected $autoWriteTimestamp = true;
+    //生成token
+    public function createSaveToken($arr = [])
+    {
+        //生成token
+        $token = sha1(md5(uniqid(md5(microtime(true)), true)));
+        $arr['token'] = $token;
+        //登录过期时间
+        $expire = array_key_exists('expires_in', $arr) ? $arr['expires_in'] : config('api.token_expire');
+        //保存到缓存中
+        if (!cache($token, $arr, config('api.token_expire'))) TApiException();
+        return $token;
+    }
     //用户登录
     public function Login()
     {
